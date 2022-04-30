416f423ecd1691992c33c4c010f123e7d6ec26b1
Juri Berlanda <juriberlanda@hotmail.com>
2022-04-30 16:55:21 +0200

    [Bug] Set Angular to production, add map, set height to 100%.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { latLng, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-leaflet-267';
+
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
 }

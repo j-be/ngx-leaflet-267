cdeb9b66c936c55475458336c9e990be1b345ddd
Juri Berlanda <juriberlanda@hotmail.com>
2022-04-30 16:54:40 +0200

    [Leaflet] Add to project as by documentation.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    LeafletModule,
     BrowserModule
   ],
   providers: [],
